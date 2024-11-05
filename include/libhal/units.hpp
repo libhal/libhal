b4e75512d205de2bb5e6c90681e740807fe2e378
Khalil Estell <kammce0@gmail.com>
2024-11-04 19:14:37 -0800

    :technologist: Add short integer names to hal namespace

--- include/libhal/units.hpp
+++ include/libhal/units.hpp
@@ -26,10 +26,23 @@ namespace hal {
 /// The standard time durations in libhal std::chrono::nanoseconds
 using time_duration = std::chrono::nanoseconds;
 
-/// Standard type for bytes in libhal. hal::byte has a number of annoyances that
-/// results in more verbose code without much benefit and thus hal::byte was
-/// created.
+/// Standard type for bytes in libhal.
+/// Libhal does not use `std::byte` because it has a number of annoyances that
+/// results in more verbose code, usually a lot of static_casts, without much
+/// benefit. Thus hal::byte was created.
 using byte = std::uint8_t;
+
+// Shortened versions of the standard integers. Integer names adopted from the
+// Rust and Zig.
+using u8 = std::uint8_t;
+using u16 = std::uint16_t;
+using u32 = std::uint32_t;
+using u64 = std::uint64_t;
+
+using i8 = std::int8_t;
+using i16 = std::int16_t;
+using i32 = std::int32_t;
+using i64 = std::int64_t;
 
 /// Type for frequency represented in hertz.
 using hertz = float;
