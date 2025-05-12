96559bce08da496907e36aeb71a5556bc2038072
Khalil Estell <kammce0@gmail.com>
2025-05-12 08:34:10 -0700

    Storing the current work

--- tests/coro_expr.test.cpp
+++ tests/coro_expr.test.cpp
@@ -22,14 +22,14 @@ class debug_monotonic_buffer_resource : public std::pmr::memory_resource
 private:
   void* do_allocate(std::size_t p_bytes, std::size_t p_alignment) override
   {
-    std::println("bytes = %zu, alignment = %zu\n", p_bytes, p_alignment);
+    std::println("bytes = {}, alignment = {}", p_bytes, p_alignment);
     return m_resource.allocate(p_bytes, p_alignment);
   }
   void do_deallocate(void* p_address,
                      std::size_t p_bytes,
                      std::size_t p_alignment) override
   {
-    std::println("address = %p, bytes = %zu, alignment = %zu\n",
+    std::println("address = {}, bytes = {}, alignment = {}",
                  p_address,
                  p_bytes,
                  p_alignment);
@@ -69,16 +69,74 @@ private:
 class task_promise_base
 {
 public:
+  // For regular functions
+  template<typename... Args>
+  static void* operator new(std::size_t size,
+                            coroutine_context& p_context,
+                            Args&&...)
+  {
+    return std::pmr::polymorphic_allocator<>(p_context.resource())
+      .allocate(size);
+  }
+
+  // For member functions - handles the implicit 'this' parameter
+  template<typename Class, typename... Args>
+  static void* operator new(std::size_t size,
+                            Class&,  // The 'this' object
+                            coroutine_context& p_context,
+                            Args&&...)
+  {
+    return std::pmr::polymorphic_allocator<>(p_context.resource())
+      .allocate(size);
+  }
+
+  // Custom deletion - no-op as specified by the requirements
+  static void operator delete(void*)
+  {
+    // Deallocation is handled by task
+  }
+
   task_promise_base() = default;
 
-  std::suspend_always initial_suspend() noexcept
+  // Constructor that captures the coroutine parameters
+  // This gets called automatically by the coroutine machinery
+  // task_promise_type() = default;
+
+  // Constructor for regular functions
+  task_promise_base(coroutine_context& context)
+  {
+    m_context = context;  // Store the context
+  }
+
+  // Constructor for member functions (handles 'this' parameter)
+  template<typename Class>
+  task_promise_base(Class&, coroutine_context& context)
+  {
+    m_context = context;  // Store the context
+  }
+
+  // Generic constructor for additional parameters
+  template<typename... Args>
+  task_promise_base(coroutine_context& context, Args&&...)
+  {
+    m_context = context;  // Store the context
+  }
+
+  // Constructor for member functions with additional parameters
+  template<typename Class, typename... Args>
+  task_promise_base(Class&, coroutine_context& context, Args&&...)
+  {
+    m_context = context;  // Store the context
+  }
+
+  constexpr std::suspend_always initial_suspend() noexcept
   {
     return {};
   }
 
   struct final_awaiter
   {
-    constexpr bool await_ready() noexcept
+    bool await_ready() noexcept
     {
       return false;
     }
@@ -87,13 +145,9 @@ public:
     std::coroutine_handle<> await_suspend(
       std::coroutine_handle<Promise> p_handle) noexcept
     {
-      // Perform symmetric transfer to continuation if it exists
-      task_promise_base& promise = p_handle.promise();
-      if (promise.m_continuation) {
-        return promise.m_continuation;
-      }
-      // Otherwise just return to caller
-      return std::noop_coroutine();
+      // The coroutine is now suspended at the final-suspend point.
+      // Lookup its continuation in the promise and resume it symmetrically.
+      return p_handle.promise().m_continuation;
     }
 
     void await_resume() noexcept
@@ -101,7 +155,7 @@ public:
     }
   };
 
-  final_awaiter final_suspend() noexcept
+  constexpr final_awaiter final_suspend() noexcept
   {
     return {};
   }
@@ -123,9 +177,9 @@ public:
 
   // Storage for the coroutine result/error
   std::coroutine_handle<> m_continuation{};
+  coroutine_context m_context{};
   // NOLINTNEXTLINE(bugprone-throw-keyword-missing)
   std::exception_ptr m_error{};
-  coroutine_context m_context{};
 };
 
 template<typename T>
@@ -135,34 +189,9 @@ template<typename T>
 class task_promise_type : public task_promise_base
 {
 public:
-  // For regular functions
-  template<typename... Args>
-  static void* operator new(std::size_t size,
-                            coroutine_context& p_context,
-                            Args&&...)
-  {
-    return std::pmr::polymorphic_allocator<>(p_context.resource())
-      .allocate(size);
-  }
-
-  // For member functions - handles the implicit 'this' parameter
-  template<typename Class, typename... Args>
-  static void* operator new(std::size_t size,
-                            Class&,  // The 'this' object
-                            coroutine_context& p_context,
-                            Args&&...)
-  {
-    return std::pmr::polymorphic_allocator<>(p_context.resource())
-      .allocate(size);
-  }
-
-  // Custom deletion - no-op as specified by the requirements
-  static void operator delete(void*)
-  {
-    // Deallocation is handled by task
-  }
-
-  task_promise_type() = default;
+  using task_promise_base::task_promise_base;  // Inherit constructors
+  using task_promise_base::operator new;
+  using task_promise_base::operator delete;
 
   task<T> get_return_object() noexcept;
 
@@ -176,42 +205,25 @@ public:
 
   decltype(auto) result()
   {
-    return m_value;
+    // NOLINTNEXTLINE(bugprone-unchecked-optional-access)
+    return m_value.value();
   }
 
-  T m_value{};
+  auto* optional_result_address()
+  {
+    return &m_value;
+  }
+
+  std::optional<T> m_value{};
 };
 
 template<>
 class task_promise_type<void> : public task_promise_base
 {
 public:
-  // For regular functions
-  template<typename... Args>
-  static void* operator new(std::size_t size,
-                            coroutine_context& p_context,
-                            Args&&...)
-  {
-    return std::pmr::polymorphic_allocator<>(p_context.resource())
-      .allocate(size);
-  }
-
-  // For member functions - handles the implicit 'this' parameter
-  template<typename Class, typename... Args>
-  static void* operator new(std::size_t size,
-                            Class&,  // The 'this' object
-                            coroutine_context& p_context,
-                            Args&&...)
-  {
-    return std::pmr::polymorphic_allocator<>(p_context.resource())
-      .allocate(size);
-  }
-
-  // Custom deletion - no-op as specified by the requirements
-  static void operator delete(void*)
-  {
-    // Deallocation is handled by task
-  }
+  using task_promise_base::task_promise_base;  // Inherit constructors
+  using task_promise_base::operator new;
+  using task_promise_base::operator delete;
 
   task_promise_type();
 
@@ -230,11 +242,11 @@ public:
   // Run synchronously and return result
   T result()
   {
-    if (!m_handle) {
+    if (not m_handle) {
       throw std::runtime_error("Task has no associated coroutine");
     }
 
-    while (!m_handle.done()) {
+    while (not m_handle.done()) {
       m_handle.resume();
     }
 
@@ -255,7 +267,7 @@ public:
 
     [[nodiscard]] bool await_ready() const noexcept
     {
-      return !m_handle || m_handle.done();
+      return not m_handle || m_handle.done();
     }
 
     // Generic await_suspend for any promise type
@@ -263,31 +275,13 @@ public:
     std::coroutine_handle<> await_suspend(
       std::coroutine_handle<Promise> p_continuation) noexcept
     {
-      m_handle.promise().set_continuation(p_continuation);
-      return m_handle;
-    }
-
-    // Specialized await_suspend for our promise type - propagates allocator
-    std::coroutine_handle<> await_suspend(
-      std::coroutine_handle<promise_type> p_continuation) noexcept
-    {
-      // Get allocator from parent coroutine
-      auto* parent_allocator = p_continuation.promise().get_allocator();
-
-      // Propagate allocator to child coroutine
-      if (parent_allocator) {
-        m_handle.promise().set_allocator(parent_allocator);
-      }
-
-      // Store continuation for resumption
-      m_handle.promise().set_continuation(p_continuation);
-
+      m_handle.promise().m_continuation = p_continuation;
       return m_handle;
     }
 
     T await_resume()
     {
-      if (!m_handle) {
+      if (not m_handle) {
         throw std::runtime_error("Awaiting a null task");
       }
 
@@ -309,11 +303,12 @@ public:
     : m_handle(std::exchange(p_other.m_handle, {}))
   {
   }
+
   ~task()
   {
     if (m_handle) {
       // auto& context = m_handle.promise().m_context;
-      m_handle.destroy();
+      // m_handle.destroy();
       // context.resource()->deallocate(m_handle.address(), 1);
     }
   }
@@ -355,8 +350,10 @@ template<class Class, typename Ret, typename... Args>
 class forward_context
 {
 private:
-  std::coroutine_handle<hal::task_promise_type<Ret>> m_handle;
+  // Store return value
+  std::optional<Ret>* m_result_ptr = nullptr;
 
+  // Class this API belongs to
   Class* m_instance;
   // Function pointer to invoke the member function with type erasure
   hal::task<Ret> (*m_invoke_fn)(Class*, hal::coroutine_context&, Args...);
@@ -397,22 +394,15 @@ public:
     // Get allocator from parent coroutine
     auto context = p_continuation.promise().context();
     auto new_task = invoke(context);
-    m_handle = new_task.handle();
-    m_handle.promise().m_continuation = p_continuation;
-    return m_handle;
+    new_task.handle().promise().m_continuation = p_continuation;
+    m_result_ptr = new_task.handle().promise().optional_result_address();
+    // symmetric transfer!!
+    return new_task.handle();
   }
 
   Ret await_resume()
   {
-    if (!m_handle) {
-      throw std::runtime_error("Awaiting a null task");
-    }
-
-    if constexpr (std::is_void_v<Ret>) {
-      m_handle.promise().result();
-    } else {
-      return m_handle.promise().result();
-    }
+    return m_result_ptr->value();  // NOLINT
   }
 };
 
@@ -421,13 +411,27 @@ class interface
 {
 public:
   // Example method that returns a captured call
+  auto evaluate(hal::coroutine_context& p_resource,
+                std::span<hal::byte const> p_data)
+  {
+    return driver_evaluate(p_resource, p_data);
+  }
+
+  // Example method that returns a captured call
   auto evaluate(std::span<hal::byte const> p_data)
   {
     return forward_context(this, &interface::deferred_evaluate, p_data);
   }
 
   // Example method that returns a captured call
-  auto sum(std::span<hal::byte const> p_data)
+  auto hash(hal::coroutine_context& p_resource,
+            std::span<hal::byte const> p_data)
+  {
+    return driver_hash(p_resource, p_data);
+  }
+
+  // Example method that returns a captured call
+  auto hash(std::span<hal::byte const> p_data)
   {
     return forward_context(this, &interface::deferred_hash, p_data);
   }
@@ -453,13 +457,11 @@ private:
   {
     return p_instance->driver_hash(p_resource, p_data);
   }
+
   virtual hal::task<hal::usize> driver_hash(
     hal::coroutine_context& p_resource,
     std::span<hal::byte const> p_data) = 0;
 };
-
-hal::debug_monotonic_buffer_resource<1024> buff;
-hal::coroutine_context context(&buff);
 
 class my_implementation : public interface
 {
@@ -495,36 +497,39 @@ private:
   {
     auto const sum = co_await m_impl->evaluate(p_data);
     // Implementation here
-    co_return sum* p_data.size();
+    co_return sum + p_data.size();
   }
 
   hal::task<hal::usize> driver_hash(hal::coroutine_context& p_resource,
                                     std::span<hal::byte const> p_data) override
   {
     unsigned const sum = std::accumulate(p_data.begin(), p_data.end(), 0);
-    co_return sum* p_data.size();
+    auto const eval = co_await driver_evaluate(p_resource, p_data);
+    co_return sum + eval + p_data.size();
   }
 
   interface* m_impl = nullptr;
 };
 
-// Example usage
-auto example(interface& p_impl, std::span<hal::byte> p_buff)
-{
-  auto call = p_impl.evaluate(p_buff);
-  return call.invoke(context);
-}
-
-hal::task<hal::usize> my_task(hal::coroutine_context&,
+hal::task<hal::usize> my_task(hal::coroutine_context& p_context,
                               interface& p_impl,
                               std::span<hal::byte> p_buff)
 {
-  auto value = co_await p_impl.evaluate(p_buff);
-  auto value1 = co_await p_impl.evaluate(p_buff);
-  auto value2 = co_await p_impl.evaluate(p_buff);
-
+#if 0
+  auto value = co_await p_impl.evaluate(p_context, p_buff);
+  auto value1 = co_await p_impl.evaluate(p_context, p_buff);
+  auto value2 = co_await p_impl.evaluate(p_context, p_buff);
   co_return value + value1 + value2;
+#else
+  auto value = co_await p_impl.hash(p_context, p_buff);
+  auto value1 = co_await p_impl.hash(p_context, p_buff);
+  auto value2 = co_await p_impl.hash(p_context, p_buff);
+  co_return value + value1 + value2;
+#endif
 }
+
+hal::debug_monotonic_buffer_resource<1024UZ * 10> buff;
+hal::coroutine_context context(&buff);
 
 boost::ut::suite<"coro_exper"> coro_expr = []() {
   using namespace boost::ut;
@@ -534,12 +539,14 @@ boost::ut::suite<"coro_exper"> coro_expr = []() {
 
     std::array<hal::byte, 5> special_buffer{};
 
-    auto handle = example(impl2, special_buffer);
-    auto value = handle.result();
-    std::println("value = %ld\n", value);
+    auto handle = my_task(context, impl2, special_buffer).handle();
 
-    auto handle2 = my_task(context, impl2, special_buffer);
-    auto value2 = handle.result();
-    std::println("value2 = %ld\n", value2);
+    while (not handle.done()) {
+      handle.resume();
+    }
+
+    auto value = handle.promise().result();
+    // auto value2 = handle2.result();
+    std::println("value = {}", value);
   };
 };
