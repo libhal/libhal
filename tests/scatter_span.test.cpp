76443c23f199e97e366650cc6978962eddf5a345
Khalil Estell <khalil.estell@sjsu.edu>
2025-08-15 18:42:10 -0700

    :sparkles: (minor) Add scatter_span concepts (#160)

--- tests/scatter_span.test.cpp
+++ tests/scatter_span.test.cpp
@@ -22,7 +22,7 @@
 
 #include <boost/ut.hpp>
 
-namespace hal {
+namespace hal::v5 {
 namespace {
 struct multi_buffer
 {
@@ -37,7 +37,7 @@ boost::ut::suite scatter_api_test = []() {
   "make_scatter_array - generic"_test = [] {
     multi_buffer buf;
     constexpr auto expected_length = 2;
-    auto arr = hal::v5::make_scatter_array<int const>(buf.arr, buf.vec);
+    auto arr = make_scatter_array<int const>(buf.arr, buf.vec);
 
     static_assert(
       std::is_same_v<decltype(arr),
@@ -57,8 +57,8 @@ boost::ut::suite scatter_api_test = []() {
     std::vector<hal::byte> v{ 0x5, 0x6 };
     constexpr static std::array<hal::byte, 3> static_arr{ 0xA, 0xB, 0xC };
 
-    auto arr = hal::v5::make_scatter_bytes(a, v, static_arr);
-    auto scatter_span = hal::v5::scatter_span<hal::byte const>(arr);
+    auto arr = make_scatter_bytes(a, v, static_arr);
+    auto span = scatter_span<hal::byte const>(arr);
 
     static_assert(
       std::is_same_v<decltype(arr), std::array<std::span<hal::byte const>, 3>>,
@@ -72,13 +72,13 @@ boost::ut::suite scatter_api_test = []() {
     expect(that % 0xB == arr[2][1]);
     expect(that % 0xC == arr[2][2]);
 
-    expect(that % 0x1 == scatter_span[0][0]);
-    expect(that % 0x4 == scatter_span[0][3]);
-    expect(that % 0x5 == scatter_span[1][0]);
-    expect(that % 0x6 == scatter_span[1][1]);
-    expect(that % 0xA == scatter_span[2][0]);
-    expect(that % 0xB == scatter_span[2][1]);
-    expect(that % 0xC == scatter_span[2][2]);
+    expect(that % 0x1 == span[0][0]);
+    expect(that % 0x4 == span[0][3]);
+    expect(that % 0x5 == span[1][0]);
+    expect(that % 0x6 == span[1][1]);
+    expect(that % 0xA == span[2][0]);
+    expect(that % 0xB == span[2][1]);
+    expect(that % 0xC == span[2][2]);
 
     using element_type = std::remove_reference_t<decltype(arr[0][0])>;
     static_assert(std::is_const_v<element_type>, "Element should be non-const");
@@ -88,8 +88,8 @@ boost::ut::suite scatter_api_test = []() {
     std::array<hal::byte, 3> a{ 0x7, 0x8, 0x9 };
     std::vector<hal::byte> v{ 0xA, 0xB };
 
-    auto arr = hal::v5::make_writable_scatter_bytes(a, v);
-    auto scatter_span = hal::v5::scatter_span<hal::byte>(arr);
+    auto arr = make_writable_scatter_bytes(a, v);
+    auto span = scatter_span<hal::byte>(arr);
 
     static_assert(
       std::is_same_v<decltype(arr), std::array<std::span<hal::byte>, 2>>,
@@ -99,10 +99,51 @@ boost::ut::suite scatter_api_test = []() {
 
     expect(that % 0xC == arr[0][0]);
     expect(that % 0xC == a[0]);
-    expect(that % 0xC == scatter_span[0][0]);
+    expect(that % 0xC == span[0][0]);
 
     static_assert(not std::is_const_v<decltype(arr[0][0])>,
                   "Element should be non-const");
   };
+
+  "compile time testing of spanable concepts"_test = [] {
+    struct test_struct
+    {};
+
+    static_assert(spanable<std::span<hal::byte>>);
+    static_assert(spanable<std::span<hal::byte const>>);
+    static_assert(spanable<std::vector<hal::byte>>);
+    static_assert(spanable<std::array<hal::byte, 10>>);
+    static_assert(spanable<std::span<test_struct>>);
+    static_assert(spanable<std::vector<int>>);
+    static_assert(spanable<std::array<float, 10>>);
+    static_assert(not spanable<test_struct>);
+    static_assert(not spanable<int>);
+    static_assert(not spanable<float>);
+    static_assert(not spanable<test_struct>);
+
+    static_assert(spanable_bytes<std::span<hal::byte>>);
+    static_assert(spanable_bytes<std::span<hal::byte const>>);
+    static_assert(spanable_bytes<std::vector<hal::byte>>);
+    static_assert(spanable_bytes<std::array<hal::byte, 10>>);
+    static_assert(not spanable_bytes<std::span<test_struct>>);
+    static_assert(not spanable_bytes<std::vector<int>>);
+    static_assert(not spanable_bytes<std::array<float, 10>>);
+    static_assert(not spanable_bytes<test_struct>);
+    static_assert(not spanable_bytes<int>);
+    static_assert(not spanable_bytes<float>);
+    static_assert(not spanable_bytes<test_struct>);
+
+    static_assert(spanable_writable_bytes<std::span<hal::byte>>);
+    static_assert(not spanable_writable_bytes<std::span<hal::byte const>>);
+    static_assert(spanable_writable_bytes<std::vector<hal::byte>>);
+    static_assert(spanable_writable_bytes<std::array<hal::byte, 10>>);
+    static_assert(not spanable_writable_bytes<std::span<test_struct>>);
+    static_assert(not spanable_writable_bytes<std::vector<int>>);
+    static_assert(not spanable_writable_bytes<std::array<float, 10>>);
+    static_assert(not spanable_writable_bytes<test_struct>);
+    static_assert(not spanable_writable_bytes<int>);
+    static_assert(not spanable_writable_bytes<float>);
+    static_assert(not spanable_writable_bytes<test_struct>);
+  };
 };
-}  // namespace hal
+}  // namespace hal::v5
